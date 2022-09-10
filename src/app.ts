83045873a7a8ec3dc115527c01c2fbf16b6cfdfa
Rajmal1 <aeis.gui@hotmail.com>
2022-09-09 22:45:15 -0300

    private and public keywords

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
 const accouting = new Department("Accounting");
 
+accouting.addEmployee("Max");
+accouting.addEmployee("Manu");
+// accouting.employees[2] = 'Anna';
+
 console.log(accouting);
+
+accouting.describe();
+accouting.printEmployeeInfo();
+
+// const accoutingCopy = { name: "DUMMY", describe: accouting.describe };
+
+// accoutingCopy.describe();
