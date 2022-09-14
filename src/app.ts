8a040d11d350259b5ac7add36ce7d7de87c317f0
Rajmal1 <aeis.gui@hotmail.com>
2022-09-13 23:32:15 -0300

    Optional Parameters & Properties

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
 interface Named {
-  readonly name: string;
+  readonly name?: string;
+  outputName?: string;
 }
 
 interface Greetable extends Named {
@@ -7,21 +18,29 @@ interface Greetable extends Named {
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age: number;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
     this.age = 30;
   }
 
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi!");
+    }
   }
 }
 
 let user1: Greetable;
 
-user1 = new Person("Max");
+user1 = new Person();
 
 user1.greet("Hi there - I am");
+
+console.log(user1);
